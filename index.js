fd676b80dc37f114446b256cfbf0547904f5c3f3
Gabriel Wu <qqbbnease1004@126.com>
2018-10-16 07:03:14 +0800

    can serve favicon.ico correctly

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const debug = require('debug')('server:index');
 const Koa = require('koa');
+const favicon = require('koa-favicon');
 const app = new Koa();
 
 const {route} = require('./router');
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.use(async (ctx) => {
   debug(`Request for ${ctx.path} received.`); // Output the requested path.
